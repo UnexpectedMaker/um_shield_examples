e97653c05bbcbc3443c93f529a4e638de0112d0d
Seon Rozenblum <seon@unexpectedmaker.com>
2024-08-25 08:42:13 +1000

    Added Storage Shield example and model and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# um_shield_examples
+# Unexpected Maker Shield Examples
+This repo includes some example code for various Unexpected Maker shields as well as 3D models in STEP format for a range of Unexpected Maker shields.
+
+For more information about shields available from Unexpected Maker, please visit:
+https://unexpectedmaker.com/shields
---
+++ arduino/Storage Shield/Storage_Shield_eMMC/Storage_Shield_eMMC.ino
@@ -0,0 +1,303 @@
+/*
+ * Unexpected Maker Storage Shield Example
+ * https://unexpectedmaker.com/storage-shield
+ * 
+ * eMMC and uSD Card text example
+ * 
+ * Compatible with:
+ * TinyPICO https://unexpectedmaker.com/TinyPICO
+ * TinyS3   https://unexpectedmaker.com/TinyS3
+ * ProS3   https://unexpectedmaker.com/ProS3
+ * 
+ * 2024 Unexpected Maker
+ */
+
+#include <UMS3.h>
+
+UMS3 ums3;
+
+#include "FS.h"
+#include "SD_MMC.h"
+#include "SD.h"
+#include "SPI.h"
+
+// eMMC pin mapping for TinyS3/ProS3
+const int eMMC_clk = 6;
+const int eMMC_cmd = 7;
+const int eMMC_d0 = 4;
+const int eMMC_d1 = 3;
+const int eMMC_d2 = 2;
+const int eMMC_d3 = 5;
+
+const int IO_SD_CS = 34; // uSD Card CS pin for TinyS3/ProS3
+
+/*
+// eMMC pin mapping for TinyPICO
+const int eMMC_clk = 33;
+const int eMMC_cmd = 32;
+const int eMMC_d0 = 27;
+const int eMMC_d1 = 15;
+const int eMMC_d2 = 14;
+const int eMMC_d3 = 26;
+
+const int IO_SD_CS = 5; // uSD Card CS pin for TinyPICO
+*/
+
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels) {
+  Serial.printf("Listing directory: %s\n", dirname);
+
+  File root = fs.open(dirname);
+  if (!root) {
+    Serial.println("Failed to open directory");
+    return;
+  }
+  if (!root.isDirectory()) {
+    Serial.println("Not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      if (levels) {
+        listDir(fs, file.path(), levels - 1);
+      }
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("  SIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
+
+void createDir(fs::FS &fs, const char *path) {
+  Serial.printf("Creating Dir: %s\n", path);
+  if (fs.mkdir(path)) {
+    Serial.println("Dir created");
+  } else {
+    Serial.println("mkdir failed");
+  }
+}
+
+void removeDir(fs::FS &fs, const char *path) {
+  Serial.printf("Removing Dir: %s\n", path);
+  if (fs.rmdir(path)) {
+    Serial.println("Dir removed");
+  } else {
+    Serial.println("rmdir failed");
+  }
+}
+
+void readFile(fs::FS &fs, const char *path) {
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+}
+
+void renameFile(fs::FS &fs, const char *path1, const char *path2) {
+  Serial.printf("Renaming file %s to %s\n", path1, path2);
+  if (fs.rename(path1, path2)) {
+    Serial.println("File renamed");
+  } else {
+    Serial.println("Rename failed");
+  }
+}
+
+void deleteFile(fs::FS &fs, const char *path) {
+  Serial.printf("Deleting file: %s\n", path);
+  if (fs.remove(path)) {
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+}
+
+void testFileIO(fs::FS &fs, const char *path) {
+  File file = fs.open(path);
+  static uint8_t buf[512];
+  size_t len = 0;
+  uint32_t start = millis();
+  uint32_t end = start;
+  if (file) {
+    len = file.size();
+    size_t flen = len;
+    start = millis();
+    while (len) {
+      size_t toRead = len;
+      if (toRead > 512) {
+        toRead = 512;
+      }
+      file.read(buf, toRead);
+      len -= toRead;
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes read for %u ms\n", flen, end);
+    file.close();
+  } else {
+    Serial.println("Failed to open file for reading");
+  }
+
+
+  file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  size_t i;
+  start = millis();
+  for (i = 0; i < 2048; i++) {
+    file.write(buf, 512);
+  }
+  end = millis() - start;
+  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+  file.close();
+}
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Initialize all board peripherals, call this first
+  ums3.begin();
+
+  // Brightness is 0-255. We set it to 1/3 brightness here
+  ums3.setPixelBrightness(255 / 3);
+
+  // Enable the power to the RGB LED.
+  // Off by default so it doesn't use current when the LED is not required.
+  ums3.setPixelPower(true);
+
+  ums3.setPixelColor(UMS3::color(0, 0, 255));
+
+  delay(2000);
+
+  if (!SD_MMC.setPins(eMMC_clk, eMMC_cmd, eMMC_d0, eMMC_d1, eMMC_d2, eMMC_d3)) {
+    Serial.println("eMMC SetPins Failed");
+  }
+
+  //    bool begin(const char * mountpoint="/sdcard", bool mode1bit=false, bool format_if_mount_failed=false, int sdmmc_frequency=BOARD_MAX_SDMMC_FREQ, uint8_t maxOpenFiles = 5);
+
+
+  if (!SD_MMC.begin("/sdcard", false, true, BOARD_MAX_SDMMC_FREQ, 5)) {
+    Serial.println("Card Mount Failed");
+    ums3.setPixelColor(UMS3::color(255, 0, 0));
+    return;
+  } else {
+    Serial.println("Card Mount worked?");
+    ums3.setPixelColor(UMS3::color(0, 255, 0));
+  }
+  uint8_t cardType = SD_MMC.cardType();
+
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD_MMC card attached");
+    return;
+  }
+
+  Serial.print("SD_MMC Card Type: ");
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
+  Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
+
+  listDir(SD_MMC, "/", 0);
+  createDir(SD_MMC, "/mydir");
+  listDir(SD_MMC, "/", 0);
+  removeDir(SD_MMC, "/mydir");
+  listDir(SD_MMC, "/", 2);
+  writeFile(SD_MMC, "/hello.txt", "Hello ");
+  appendFile(SD_MMC, "/hello.txt", "World!\n");
+  readFile(SD_MMC, "/hello.txt");
+  deleteFile(SD_MMC, "/foo.txt");
+  renameFile(SD_MMC, "/hello.txt", "/foo.txt");
+  readFile(SD_MMC, "/foo.txt");
+  testFileIO(SD_MMC, "/test.txt");
+  Serial.printf("Total space: %lluMB\n", SD_MMC.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD_MMC.usedBytes() / (1024 * 1024));
+
+
+  Serial.println("\n** Starting uSD Card Test\n");
+
+  if (!SD.begin(IO_SD_CS))
+  {
+    Serial.println("uSD Card Mount Failed");
+    return;
+  }
+  cardType = SD.cardType();
+
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+  listDir(SD, "/", 0);
+}
+
+void loop() {
+}
